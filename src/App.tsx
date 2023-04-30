4d579a9185e253c45969634a654493d81d6ae6ed
Anton Zamay <tony.zamay@mail.ru>
2023-04-30 18:45:38 +0200

    timer

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import {Board} from "./models/Board";
 import {Player} from "./models/Player";
 import {Colors} from "./models/Colors";
 import LostFigures from "./components/LostFigures";
+import Timer from "./components/Timer";
 
 function App() {
     const [board, setBoard] = useState(new Board())
@@ -30,6 +31,7 @@ function App() {
 
     return (
       <div className="app">
+        <Timer currentPlayer={currentPlayer} restart={restart} />
         <BoardComponent
             board={board}
             setBoard={setBoard}
